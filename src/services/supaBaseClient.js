6686847b9e3e52aed251ec37fb7b67a031798940
yulian168 <ym67896@gmail.com>
2026-07-03 04:36:22 -0500

    importacion y creacion de cliente supabase

--- src/services/supaBaseClient.js
+++ src/services/supaBaseClient.js
@@ -1,4 +1,6 @@
+import {createClient} from '@supabase/supabase-js'
 
-export async function peticion(){
-    
-}
+const supabaseURL="https://temporal.supabase.co"//despues le colocamos las variables de entorno de los .env
+const supabaseKey="ejemploKeyTemporal_4243"//solo es definiendo estructura
+
+export const peticionSupa= createClient(supabaseURL, supabaseKey)
